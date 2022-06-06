bfa1c603477aa3b3a29539ae5331ff3908a080de
Ashley Battles <ashbatt17@yahoo.com>
2022-06-05 20:21:35 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log('Ashley for the WIN$$$$$$$')
+
+
+function alertNewYork(){
+    // setup an alert for each city // city girl hahaha
+    alert("Lets be shady in the city!!!");
+}
+
+function alertCali(){
+    // setup an alert for each city // city girl hahaha
+    alert("Not another EARTHQUAKE!!!!");
+}
+
+function alertVegas() {
+    alert("What a day to be alive! NOW LET'S GAMBLE!!!!!!")
+}
+
+function hideCookie() {
+    const element = document.querySelector('.contain-cookie')
+    element.remove();
+}
